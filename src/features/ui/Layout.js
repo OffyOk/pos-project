545abcc1abed3ac9ec4ccf74a1487281ee59ef1a
OffyOk <96122242+OffyOk@users.noreply.github.com>
2024-05-02 23:42:55 +0700

    Fix Footer

--- src/features/ui/Layout.js
+++ src/features/ui/Layout.js
@@ -10,13 +10,7 @@ import {
 import { useDispatch, useSelector } from 'react-redux';
 import { capitalize } from 'lodash';
 import { clearAlert } from './uiSlice';
-import {
-  MDBFooter,
-  MDBContainer,
-  MDBRow,
-  MDBCol,
-  MDBIcon,
-} from 'mdb-react-ui-kit';
+import { MDBFooter, MDBContainer } from 'mdb-react-ui-kit';
 
 const Layout = () => {
   const dispatch = useDispatch();
@@ -71,66 +65,20 @@ const Layout = () => {
         </Toast>
       </ToastContainer>
       <MDBFooter
-        bgColor="Dark"
-        className="text-center text-lg-start text-muted"
+        className="text-center text-white fixed-bottom "
+        style={{ backgroundColor: '#212529' }}
       >
-        <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-          <div className="me-5 d-none d-lg-block">
-            <span>Get connected with us on social networks:</span>
-          </div>
-
-          <div>
-            <a
-              href="www.linkedin.com/in/suthichai-srivatanasup"
-              className="me-4 text-reset"
-            >
-              <MDBIcon fab icon="linkedin" />
-            </a>
-            <a href="https://github.com/OffyOk" className="me-4 text-reset">
-              <MDBIcon fab icon="github" />
-            </a>
-          </div>
-        </section>
-
-        <section className="">
-          <MDBContainer className="text-center text-md-start mt-5">
-            <MDBRow className="mt-3">
-              <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">
-                  <MDBIcon icon="gem" className="me-3" />
-                  Company name
-                </h6>
-                <p>
-                  Here you can use rows and columns to organize your footer
-                  content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                  elit.
-                </p>
-              </MDBCol>
-
-              <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
-                <p>
-                  <MDBIcon icon="home" className="me-2" />
-                  New York, NY 10012, US
-                </p>
-                <p>
-                  <MDBIcon icon="envelope" className="me-3" />
-                  ssrivattanasup@gmail.com
-                </p>
-                <p>
-                  <MDBIcon icon="phone" className="me-3" /> + 01 234 567 88
-                </p>
-              </MDBCol>
-            </MDBRow>
-          </MDBContainer>
-        </section>
+        <MDBContainer className="p-1"></MDBContainer>
 
         <div
-          className="text-center p-4"
-          style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}
+          className="text-center p-3"
+          style={{ backgroundColor: 'rgba(33, 37, 41, 1)' }}
         >
           © 2024 Copyright:
-          <a className="text-reset fw-bold" href="https://github.com/OffyOk">
+          <a
+            className="text-white"
+            href="https://www.linkedin.com/in/suthichai-srivatanasup/"
+          >
             OffyOk
           </a>
         </div>
